929e79998469f7cd902006d475774a3a6813401d
lynguyen <lynguyen.embedded@gmail.com>
2026-08-04 18:20:07 +0700

    Add signal

---
+++ feature/OSAL/OSAL_Event.c
@@ -0,0 +1,202 @@
+#include <asm-generic/errno.h>
+#include <bits/pthread_types.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "NCL_Core.h"
+#include "NCL_Types.h"
+#include "OSAL_Event.h"
+#include "OSAL_Memory.h"
+#include "OSAL_Mutex.h"
+
+NCL_ERRORTYPE NCL_OSAL_SignalCreate(NCL_HANDLETYPE *eventHandle) {
+  NCL_ERRORTYPE ret = NCL_ErrorNone;
+  NCL_OSAL_THREADEVENT *event;
+  int mutex_ret = 0;
+  int cond_ret = 0;
+
+  if (!eventHandle) {
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  event = (NCL_OSAL_THREADEVENT *)NCL_OSAL_Malloc(sizeof(NCL_OSAL_THREADEVENT));
+  if (!event) {
+    ret = NCL_ErrorInsufficientResources;
+    goto EXIT;
+  }
+  event->m_signal = NCL_FALSE;
+
+  mutex_ret = NCL_OSAL_MutexCreate(&event->m_mutex);
+  if (!mutex_ret) {
+    printf("[NCL_OSAL_SignalCreate]: Mutex create failed - %d\n", mutex_ret);
+    NCL_OSAL_Free(event);
+    ret = NCL_ErrorInsufficientResources;
+    goto EXIT;
+  }
+
+  cond_ret = pthread_cond_init(&event->m_condition, NULL);
+  if (!cond_ret) {
+    printf("[NCL_OSAL_SignalCreate]: Cond create failed - %d\n", cond_ret);
+    NCL_OSAL_MutexTerminate(event->m_mutex);
+    NCL_OSAL_Free(event);
+    ret = NCL_ErrorInsufficientResources;
+    goto EXIT;
+  }
+
+  *eventHandle = (NCL_HANDLETYPE)event;
+  ret = NCL_ErrorNone;
+  goto EXIT;
+
+EXIT:
+  printf("[NCL_OSAL_SignalCreate] : ret %d\n", ret);
+  return ret;
+}
+
+NCL_ERRORTYPE NCL_OSAL_SignalTerminate(NCL_HANDLETYPE eventHandle) {
+  NCL_ERRORTYPE ret = NCL_ErrorNone;
+  int condTerminate_ret = 0;
+  int mutexTerminate_ret = 0;
+  NCL_OSAL_THREADEVENT *event = (NCL_OSAL_THREADEVENT *)eventHandle;
+
+  if (!event) {
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  condTerminate_ret = pthread_cond_destroy(&event->m_condition);
+  if (condTerminate_ret != 0) {
+    ret = NCL_ErrorUndefined;
+    printf("[NCL_OSAL_SignalTerminate] - cond destroy failed %d\n",
+           condTerminate_ret);
+    NCL_OSAL_Free(event);
+    goto EXIT;
+  }
+
+  mutexTerminate_ret = NCL_OSAL_MutexTerminate(event->m_mutex);
+  if (mutexTerminate_ret != 0) {
+    ret = NCL_ErrorUndefined;
+    printf("[NCL_OSAL_SignalTerminate] - mutex destroy failed %d\n",
+           mutexTerminate_ret);
+    NCL_OSAL_Free(event);
+    goto EXIT;
+  }
+
+  ret = NCL_ErrorNone;
+  goto EXIT;
+EXIT:
+  printf("[NCL_OSAL_SignalTerminate]: ret %d\n", ret);
+  return ret;
+}
+
+NCL_ERRORTYPE NCL_OSAL_SignalReset(NCL_HANDLETYPE eventHandle) {
+  NCL_ERRORTYPE ret = NCL_ErrorNone;
+  NCL_OSAL_THREADEVENT *event = (NCL_OSAL_THREADEVENT *)eventHandle;
+  if (!event) {
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  ret = NCL_OSAL_MutexLock(event->m_mutex);
+  if (ret != NCL_ErrorNone) {
+    printf("[NCL_OSAL_SignalReset] - mutex lock failed %d\n", ret);
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  event->m_signal = NCL_FALSE;
+  NCL_OSAL_MutexUnlock(event->m_mutex);
+
+  ret = NCL_ErrorNone;
+  goto EXIT;
+
+EXIT:
+  printf("[NCL_OSAL_SignalReset] - ret %d\n", ret);
+  return ret;
+}
+
+NCL_ERRORTYPE NCL_OSAL_SignalSet(NCL_HANDLETYPE eventHandle) {
+  NCL_ERRORTYPE ret = NCL_ErrorNone;
+  NCL_OSAL_THREADEVENT *event = (NCL_OSAL_THREADEVENT *)eventHandle;
+  if (!event) {
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  ret = NCL_OSAL_MutexLock(event->m_mutex);
+  if (ret != NCL_ErrorNone) {
+    printf("[NCL_OSAL_SignalSet] - mutex lock failed %d\n", ret);
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  event->m_signal = NCL_TRUE;
+  pthread_cond_signal(&event->m_condition);
+
+  NCL_OSAL_MutexUnlock(event->m_mutex);
+
+  ret = NCL_ErrorNone;
+  goto EXIT;
+
+EXIT:
+  printf("[NCL_OSAL_SignalSet] - ret %d\n", ret);
+  return ret;
+}
+
+NCL_ERRORTYPE NCL_OSAL_SignalWait_ms(NCL_HANDLETYPE eventHandle, NCL_U32 ms) {
+  NCL_ERRORTYPE ret = NCL_ErrorNone;
+  NCL_OSAL_THREADEVENT *event = (NCL_OSAL_THREADEVENT *)eventHandle;
+  struct timespec timeout;
+  struct timeval now;
+  int timeWait_ret = 0;
+  long tv_us = 0;
+
+  if (!event) {
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  gettimeofday(&now, NULL);
+
+  tv_us = now.tv_usec + ((long)ms * 1000);
+  timeout.tv_sec = now.tv_sec + tv_us / 1000000;
+  timeout.tv_nsec = (tv_us % 1000000) * 1000;
+
+  ret = NCL_OSAL_MutexLock(event->m_mutex);
+  if (ret != NCL_ErrorNone) {
+    printf("[NCL_OSAL_SignalWait_ms] - mutex lock failed %d\n", ret);
+    ret = NCL_ErrorBadParameter;
+    goto EXIT;
+  }
+
+  if (ms == 0) {
+    if (!event->m_signal) {
+      ret = NCL_ErrorTimeout;
+      goto EXIT;
+    }
+  } else if (ms == MAX_WAIT_TIME) {
+    while (!event->m_signal) {
+      pthread_cond_wait(&event->m_condition, (pthread_mutex_t *)event->m_mutex);
+    }
+    ret = NCL_ErrorNone;
+    goto EXIT;
+  } else {
+    while (!event->m_signal) {
+      timeWait_ret = pthread_cond_timedwait(
+          &event->m_condition, (pthread_mutex_t *)event->m_mutex, &timeout);
+      if (!event->m_signal && timeWait_ret == ETIMEDOUT) {
+        ret = NCL_ErrorTimeout;
+        goto EXIT;
+      }
+    }
+  }
+
+  NCL_OSAL_MutexUnlock(event->m_mutex);
+  ret = NCL_ErrorNone;
+  goto EXIT;
+
+EXIT:
+  printf("[NCL_OSAL_SignalWait_ms] - ret %d\n", ret);
+  return ret;
+}
---
+++ feature/OSAL/OSAL_Event.h
@@ -0,0 +1,30 @@
+#ifndef __OSAL_EVENT_H__
+#define __OSAL_EVENT_H__
+
+#include "NCL_Core.h"
+#include "NCL_Types.h"
+#include <pthread.h>
+
+#define MAX_WAIT_TIME 0xFFFFFFFF
+
+typedef struct _NCL_OSAL_THREADEVENT {
+  NCL_BOOLEAN m_signal;
+  NCL_HANDLETYPE m_mutex;
+  pthread_cond_t m_condition;
+} NCL_OSAL_THREADEVENT;
+
+#ifdef __cplusplus // avoid overloading
+extern "C" {
+#endif
+
+NCL_ERRORTYPE NCL_OSAL_SignalCreate(NCL_HANDLETYPE *eventHandle);
+NCL_ERRORTYPE NCL_OSAL_SignalTerminate(NCL_HANDLETYPE eventHandle);
+NCL_ERRORTYPE NCL_OSAL_SignalReset(NCL_HANDLETYPE eventHandle);
+NCL_ERRORTYPE NCL_OSAL_SignalSet(NCL_HANDLETYPE eventHandle);
+NCL_ERRORTYPE NCL_OSAL_SignalWait_ms(NCL_HANDLETYPE eventHandle, NCL_U32 ms);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __OSAL_EVENT_H__
